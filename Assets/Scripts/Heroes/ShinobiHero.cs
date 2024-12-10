caf34862ff4ad4098c7ebc8f32600b9dc8405157
ilkayseki <58669159+ilkayseki@users.noreply.github.com>
2024-12-10 20:08:58 +0300

    Drone added

--- Assets/Scripts/Heroes/ShinobiHero.cs
+++ Assets/Scripts/Heroes/ShinobiHero.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "ArcherHero", menuName = "CreateHero/ArcherHero")]
-public class ArcherHero : HeroBaseData
+[CreateAssetMenu(fileName = "ShinobiHero", menuName = "CreateHero/ShinobiHero")]
+public class ShinobiHero : HeroBaseData
 {
     public override void HeroAttackSpeedPassiveUpgrade(float value)
     {
-        Debug.LogError("ArcherHero!");
+        Debug.LogError("ShinobiHero!");
     }
 }
